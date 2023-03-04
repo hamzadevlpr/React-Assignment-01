5c0db771d2ddec9b256c905648b4665be3c9686d
hamzadvlpr1 <muhammadhamzamlk@gmail.com>
2023-03-04 16:32:23 +0500

    My Fist React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 align="center">This is First React App</h1>
+      <p class="credit"> ©
+        <script>document.write(new Date().getFullYear());</script>2023 All Rights Reserved. Made with ❤️ by Hamza Malik
+      </p>
+    </>
+  );
+}
+
+export default App;
